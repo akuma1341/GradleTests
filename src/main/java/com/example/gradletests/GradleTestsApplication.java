76ee491c971d234ecee749146622ef397c501db2
akuma1341 <akuma1341@gmail.com>
2022-03-16 15:08:10 +0300

    Initial commit

---
+++ src/main/java/com/example/gradletests/GradleTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradletests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradletests/GradleTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradletests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
